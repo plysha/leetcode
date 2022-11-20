a3331435879aae22b07352f8a444a66bb28064d2
alexanderplynin <Aleksandr.Plynin@x5.ru>
2022-11-20 18:56:25 +0500

    Add ClimbingStairs.java solution

---
+++ src/main/java/easy/ClimbingStairs.java
@@ -0,0 +1,16 @@
+package easy;
+
+public class ClimbingStairs {
+    public int climbStairs(int n) {
+        int one = 1;
+        int two = 1;
+
+        for (int i = 0; i < n - 1; i++) {
+            int tmp = one;
+            one = one + two;
+            two = tmp;
+        }
+
+        return one;
+    }
+}
