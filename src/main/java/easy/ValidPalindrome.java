3b530866f9fb3a47accd1038afd6e61439592eed
alexanderplynin <Aleksandr.Plynin@x5.ru>
2023-01-30 11:12:36 +0500

    Add more solves

--- src/main/java/easy/ValidPalindrome.java
+++ src/main/java/easy/ValidPalindrome.java
@@ -19,4 +19,30 @@ public class ValidPalindrome {
         return true;
 
     }
+
+    public boolean isPalindrome2Solution(String s) {
+        if (s.isEmpty()) {
+            return true;
+        }
+        int head = 0, tail = s.length() - 1;
+        char cHead, cTail;
+        while (head <= tail) {
+            cHead = s.charAt(head);
+            cTail = s.charAt(tail);
+            if (!Character.isLetterOrDigit(cHead)) {
+                head++;
+            } else if (!Character.isLetterOrDigit(cTail)) {
+                tail--;
+            } else {
+                if (Character.toLowerCase(cHead) != Character.toLowerCase(cTail)) {
+                    return false;
+                }
+                head++;
+                tail--;
+            }
+        }
+
+        return true;
+    }
+
 }
